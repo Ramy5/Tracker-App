e1733ad375451677ecc48ae04e8e987da0cbd134
Ramy Sabry <ramyjounior464@gmail.com>
2026-04-03 12:23:32 +0200

    feat(FONTS-002): implement external fonts in the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import "@/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "sans-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-Regular": require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
